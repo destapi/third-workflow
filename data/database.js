7c67b8052596104758b21a1f0bec7ad66ebab624
Maina, Stephen <m41na@yahoo.com>
2024-02-28 10:41:13 -0600

    sanitizing workflow inputs

--- data/database.js
+++ data/database.js
@@ -1,11 +1,14 @@
 import { MongoClient } from 'mongodb';
 
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
-const dbUser = encodeURIComponent(process.env.MONGODB_USERNAME);
-const dbPassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
-const dbName = process.env.MONGODB_DB_NAME;
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS.trim();
+const dbUser = encodeURIComponent(process.env.MONGODB_USERNAME.trim());
+const dbPassword = encodeURIComponent(process.env.MONGODB_PASSWORD.trim());
+const dbName = process.env.MONGODB_APP_NAME.trim();
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+console.log('env values', clusterAddress, dbUser, dbPassword, dbName)
+
+//const uri = `mongodb+srv://mongo-user:<password>@us-east-1-cluster.5gwkskd.mongodb.net/?retryWrites=true&w=majority&appName=us-east-1-cluster`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=us-east-1-cluster`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
